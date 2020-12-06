77130b25aceec14cd7168a35eb7a49b241ca38e6
d49lu <d49lu@student.cs.uwaterloo.ca>
2020-12-06 14:44:00 -0500

    set up player class

---
+++ player.cc
@@ -0,0 +1,50 @@
+#include "player.h"
+
+void player::changeDice(diceType newDice) {
+    dice = newDice == diceType::fair ? fairDice{} : loadedDice{};
+}
+
+std::map<Residence, std::vector<int>> player::getResidences() const {
+    return residences;
+}
+
+void player::addResources(resource: Resource, amount: int) {
+    resources[resource] += amount;
+}
+
+void player::takeResources(resource: Resource, amount: int) {
+    if (resources[resource] < amount) {
+        throw InsufficientResourceException();
+    }
+    resources[resource] -= amount;
+}
+
+void player::generateRandomResource() {
+    // ???
+}
+int player::totalResource() const {
+    int sum = 0;
+    for (auto resource : resources) {
+        sum += resource.second;
+    }
+    return;
+}
+
+void player::buildResidence(color: Color, location: int) {
+    residences[Residence::basement].emplace_back(location);
+}
+
+void player::buildRoad(location: int) {
+    roads.emplace_back(location);
+}
+
+int player::rollDice() {
+    return diceType.rollDice();
+}
+
+int player::handleGooseRoll() {
+    // ??
+}
+void player::transfer(to: Color, resource: Resource) {
+    // ??
+}
---
+++ player.h
@@ -0,0 +1,34 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+#include <map>
+#include <vector>
+#include "enums.h"
+
+class Player {
+    public:
+        void changeDice(diceType dice);
+        std::map<Residence, std::vector<int>> getResidences() const;
+        void addResources(resource: Resource, amount: int);
+        void takeResources(resource: Resource, amount: int);
+        void generateRandomResource();
+        int totalResource() const;
+        void buildResidence(color: Color, location: int);
+        void buildRoad(location: int);
+        int rollDice();
+        int handleGooseRoll();
+        void transfer(to: Color, resource: Resource);
+
+    private:
+        Color color;
+        int points;
+        std::map<Resource, int> resources;
+        std::map<Residence, std::vector<int>> residences;
+        std::vector<int> roads;
+        Dice diceType;
+        
+};
+
+class InsufficientResourceException {};
+
+#endif
