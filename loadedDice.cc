071e2c4c0e5d4a4b6d9fef419a57a429d7420bce
d49lu <d49lu@student.cs.uwaterloo.ca>
2020-12-13 02:26:12 -0500

    change cout to cerr

--- loadedDice.cc
+++ loadedDice.cc
@@ -8,7 +8,7 @@ int LoadedDice::rollDice() const {
         std::cout << "Input a roll between 2 and 12:" << std::endl;
         if (std::cin >> loadedNum) {
             if (loadedNum > 12 || loadedNum < 2) {
-                std::cout << "ERROR: Invalid roll " << loadedNum << std::endl;
+                std::cerr << "ERROR: Invalid roll " << loadedNum << std::endl;
             } else {
                 return loadedNum;
             } 
@@ -18,7 +18,7 @@ int LoadedDice::rollDice() const {
             }
             std::cin.clear();
             std::cin.ignore(256, '\n');
-            std::cout << "ERROR:  isn't a valid integer." << std::endl;
+            std::cerr << "ERROR:  isn't a valid integer." << std::endl;
         }
     }
 }
